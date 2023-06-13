7688bbc4e24413efd20482c245b4fdfbb7834e4f
ALESENLLE <senllealejandra@gmail.com>
2023-06-12 23:17:36 -0300

    Mergeando las ramas

--- gestionar_obras.py
+++ gestionar_obras.py
@@ -1,6 +1,5 @@
 from peewee import*
 import pandas as pd
-from abc import abstractmethod
 from abc import ABCMeta
 import os
 
@@ -12,7 +11,6 @@ class GestionarObra(metaclass=ABCMeta):
     #Este es el punto 4.A
     @classmethod
     def extraer_datos(self):
-
         try:
             archivo_csv = os.getcwd()+"/"+"observatorio-de-obras-urbanas.csv"
             df = pd.read_csv(archivo_csv, sep=",")
@@ -25,7 +23,6 @@ class GestionarObra(metaclass=ABCMeta):
     @classmethod
     def conectar_db(self):
         sqlite_db = SqliteDatabase(os.getcwd()+'/'+'obras_urbanas.db', pragmas={'journal_mode': 'wal'})
-
         try:
             sqlite_db.connect()
             #La base de datos se crea cuando intenta conectarse por primera vez y no la encuentra.
@@ -33,7 +30,49 @@ class GestionarObra(metaclass=ABCMeta):
         except OperationalError as e:
             print("Se ha generado un error en la conexion a la BD.", e)
             exit()
+
     #Este es el punto 4.C
-'''    @classmethod
-    def mapear_orm(self):
-'''
+    @classmethod
+    def mapear_orm(self,BaseModel,sqlite_db):
+        class EstructuraBDObras (BaseModel):
+            entorno = CharField()
+            nombre = CharField()
+            etapa = CharField()
+            tipo = CharField()
+            area_responsable = CharField()
+            descripcion = CharField()
+            monto_contrato = CharField()
+            comuna = CharField()
+            barrio = CharField()
+            direccion = CharField()
+            lat = CharField()
+            lng = CharField()
+            fecha_inicio = CharField()
+            fecha_fin_inicial = CharField()
+            plazo_meses = CharField()
+            porcentaje_avance = CharField()
+            imagen_1 = CharField()
+            imagen_2 = CharField()
+            imagen_3 = CharField()
+            imagen_4 = CharField()
+            licitacion_oferta_empresa = CharField()
+            licitacion_anio = CharField()
+            contratacion_tipo = CharField()
+            nro_contratacion = CharField()
+            cuit_contratista = CharField()
+            beneficiarios = CharField()
+            mano_obra = CharField()
+            compromiso = CharField()
+            destacada = CharField()
+            ba_elige = CharField()
+            link_entorno = CharField()
+            pliego_descarga = CharField()
+            expediente_numero = CharField()
+            estudio_ambiental_descarga = CharField()
+            financiamiento = CharField()
+
+            class Meta:
+                db_table = 'Obras Públicas'
+        sqlite_db.create_tables([EstructuraBDObras])
+                
+                
--- modelo_orm.py
+++ modelo_orm.py
@@ -4,48 +4,10 @@ from gestionar_obras import *
 
 sqlite_db=GestionarObra().conectar_db()
 class BaseModel(Model):
+
     class Meta:
-        database=GestionarObra().conectar_db()
-               
-class EstructuraBDObras (BaseModel):
-    entorno=CharField()
-    nombre=CharField()
-    etapa=CharField()
-    tipo=CharField()
-    area_responsable=CharField()
-    descripcion=CharField()
-    monto_contrato=CharField()
-    comuna=CharField()
-    barrio=CharField()
-    direccion=CharField()
-    lat=CharField()
-    lng=CharField()
-    fecha_inicio=CharField()
-    fecha_fin_inicial=CharField()
-    plazo_meses=CharField()
-    porcentaje_avance=CharField()
-    imagen_1=CharField()
-    imagen_2=CharField()
-    imagen_3=CharField()
-    imagen_4=CharField()
-    licitacion_oferta_empresa=CharField()
-    licitacion_anio=CharField()
-    contratacion_tipo=CharField()
-    nro_contratacion=CharField()
-    cuit_contratista=CharField()
-    beneficiarios=CharField()
-    mano_obra=CharField()
-    compromiso=CharField()
-    destacada=CharField()
-    ba_elige=CharField()
-    link_entorno=CharField()
-    pliego_descarga=CharField()
-    expediente_numero=CharField()
-    estudio_ambiental_descarga=CharField()
-    financiamiento=CharField()
-    class Meta:
-        db_table='Obras Públicas'
-sqlite_db.create_tables([EstructuraBDObras])
+        database = sqlite_db
+GestionarObra().mapear_orm(BaseModel,sqlite_db)
 
 class Obra ():
     def nuevo_proyecto():
