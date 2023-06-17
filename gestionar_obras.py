b728ec44a54939db07f8403ce3555fa082368113
ALESENLLE <senllealejandra@gmail.com>
2023-06-17 17:21:07 -0300

    cargado comuna

--- gestionar_obras.py
+++ gestionar_obras.py
@@ -33,96 +33,9 @@ class GestionarObra(metaclass=ABCMeta):
 
     #Este es el punto 4.C
     @classmethod
-    def mapear_orm(self,BaseModel,sqlite_db):
-        class Ebarrios (BaseModel):
-            id = IntegerField(primary_key=True)
-            nombre = CharField()
-            nro_comuna = IntegerField()
-            class Meta:
-                db_table = 'barrios'
-
-        class Eareas_responsables (BaseModel):
-            id = IntegerField(primary_key=True)
-            descripcion = CharField()
-            class Meta:
-                db_table = 'areas_responsables'
+    def mapear_orm(self,sqlite_db,lista):
+        sqlite_db.create_tables(lista)
         
-        class Ecomunas (BaseModel):
-            id = IntegerField(primary_key=True)
-            nro_comuna = IntegerField()
-            class Meta:
-                db_table = 'comunas'
-        
-        class Eempresa (BaseModel):
-            id = IntegerField(primary_key=True)
-            cuit = CharField()
-            razonSocial = CharField()
-            class Meta:
-                db_table = 'empresa'
-        
-        class Eetapas (BaseModel):
-            id = IntegerField(primary_key=True)
-            descripcion = CharField()
-            class Meta:
-                db_table = 'etapas'
-        
-        class EFuenteFinanciamiento (BaseModel):
-            id = IntegerField(primary_key=True)
-            descripcion = CharField()
-            class Meta:
-                db_table = 'fuente_financiamiento'
-        
-       
-        
-        class Etipo_contratacion (BaseModel):
-            id = IntegerField(primary_key=True)
-            descripcion = CharField()
-            class Meta:
-                db_table = 'tipo_contratacion'
-        
-        class Etipo_obra (BaseModel):
-            id = IntegerField(primary_key=True)
-            descripcion = CharField()
-            class Meta:
-                db_table = 'tipo_obra'  
-            
-        
-        class EstructuraBDObras (BaseModel):
-            id = IntegerField(primary_key=True)    
-            entorno = CharField()
-            nombre = CharField()
-            descripcion = CharField()
-            monto_contrato = IntegerField()
-            direccion = CharField()
-            fecha_inicio = DateField()
-            fecha_fin_inicial = DateField()
-            plazo_meses = IntegerField()
-            porcentaje_avance = IntegerField()
-            licitacion_ano = IntegerField()
-            nro_contratacion = CharField()
-            beneficiarios = CharField()
-            mano_obra = CharField()
-            destacada = CharField()
-            expediente_numero = CharField()
-            id_etapa = ForeignKeyField(Eetapas, backref='etapas')
-            id_empresa = ForeignKeyField(Eempresa, backref='empresa')
-            id_tipo_obra = ForeignKeyField(Etipo_obra, backref='tipo_obra')
-            id_areas_responsables = ForeignKeyField(Eareas_responsables, backref='areas_responsables')
-            id_barrio = ForeignKeyField(Ebarrios, backref='barrios')
-            id_tipo_contratacion = ForeignKeyField(Etipo_contratacion, backref='tipo_contratacion')
-            id_fuente_financiamiento = ForeignKeyField(EFuenteFinanciamiento, backref='fuente_financiamiento')
-            
-            class Meta:
-                db_table = 'Obras Públicas'
-        class Eimagen (BaseModel):
-            id = IntegerField(primary_key=True)
-            id_obra = ForeignKeyField(EstructuraBDObras, backref='obra')
-            descripcion = CharField()
-            class Meta:
-                db_table = 'imagen'
-                
-        sqlite_db.create_tables([EstructuraBDObras,Ebarrios,Eareas_responsables,Ecomunas,Eempresa,Eetapas,EFuenteFinanciamiento,Eimagen,Etipo_contratacion,Etipo_obra])
-        return EstructuraBDObras
     
     #Este es el punto 4.D
     @classmethod
@@ -135,6 +48,21 @@ class GestionarObra(metaclass=ABCMeta):
         print(df)
         return df
     
+    #Este es el punto 4.e
+    @classmethod
+    def cargar_datos(self,Ecomunas):
+        df=self.limpiar_datos()
+        data_unique = list(df['comuna'].unique())
+        print(data_unique)
+        for elem in data_unique:
+            print("Elemento:", elem)
+            try:
+                Ecomunas.create(nro_comuna=elem)
+            except IntegrityError as e:
+                print("Error al insertar un nuevo registro en la tabla tipos_transporte.", e)
+        print("Se han persistido los tipos de transporte en la BD.")
+            
+
     
         
     
--- modelo_orm.py
+++ modelo_orm.py
@@ -6,8 +6,83 @@ class BaseModel(Model):
 
     class Meta:
         database = sqlite_db
-GestionarObra().mapear_orm(BaseModel,sqlite_db)
+class Ebarrios (BaseModel):
+            nombre = CharField()
+            nro_comuna = IntegerField()
+            class Meta:
+                db_table = 'barrios'
 
+class Eareas_responsables (BaseModel):
+            descripcion = CharField()
+            class Meta:
+                db_table = 'areas_responsables'
+class Ecomunas (BaseModel):
+            nro_comuna = IntegerField()
+            class Meta:
+                db_table = 'comunas'
+        
+class Eempresa (BaseModel):
+            cuit = CharField()
+            razonSocial = CharField()
+            class Meta:
+                db_table = 'empresa'
+        
+class Eetapas (BaseModel):
+            descripcion = CharField()
+            class Meta:
+                db_table = 'etapas'
+        
+class EFuenteFinanciamiento (BaseModel):
+            descripcion = CharField()
+            class Meta:
+                db_table = 'fuente_financiamiento'
+        
+       
+        
+class Etipo_contratacion (BaseModel):
+            descripcion = CharField()
+            class Meta:
+                db_table = 'tipo_contratacion'
+        
+class Etipo_obra (BaseModel):
+            descripcion = CharField()
+            class Meta:
+                db_table = 'tipo_obra'  
+            
+        
+class EstructuraBDObras (BaseModel):    
+            entorno = CharField()
+            nombre = CharField()
+            descripcion = CharField()
+            monto_contrato = IntegerField()
+            direccion = CharField()
+            fecha_inicio = DateField()
+            fecha_fin_inicial = DateField()
+            plazo_meses = IntegerField()
+            porcentaje_avance = IntegerField()
+            licitacion_ano = IntegerField()
+            nro_contratacion = CharField()
+            beneficiarios = CharField()
+            mano_obra = CharField()
+            destacada = CharField()
+            expediente_numero = CharField()
+            id_etapa = ForeignKeyField(Eetapas, backref='etapas')
+            id_empresa = ForeignKeyField(Eempresa, backref='empresa')
+            id_tipo_obra = ForeignKeyField(Etipo_obra, backref='tipo_obra')
+            id_areas_responsables = ForeignKeyField(Eareas_responsables, backref='areas_responsables')
+            id_barrio = ForeignKeyField(Ebarrios, backref='barrios')
+            id_tipo_contratacion = ForeignKeyField(Etipo_contratacion, backref='tipo_contratacion')
+            id_fuente_financiamiento = ForeignKeyField(EFuenteFinanciamiento, backref='fuente_financiamiento')
+            
+            class Meta:
+                db_table = 'Obras Públicas'
+class Eimagen (BaseModel):
+            id_obra = ForeignKeyField(EstructuraBDObras, backref='obra')
+            descripcion = CharField()
+            class Meta:
+                db_table = 'imagen'
+lista=[Ebarrios,Eareas_responsables,Ecomunas,Eempresa,Eetapas,Eimagen,EstructuraBDObras,Etipo_obra,Etipo_contratacion,EFuenteFinanciamiento]
+GestionarObra().mapear_orm(sqlite_db,lista)
 class Obra ():
     def nuevo_proyecto():
         pass
@@ -15,7 +90,7 @@ class Obra ():
         pass
 
 
-GestionarObra().limpiar_datos()
+GestionarObra().cargar_datos(Ecomunas)
 
 
 
